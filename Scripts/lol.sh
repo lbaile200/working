921df5b71fb8c14fa6ef08acb2db62110ff716de
Lucas W. Bailey <lbaile200@gmail.com>
2014-11-14 11:21:31 -0500

    Delete lol.sh

--- Scripts/lol.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WINEPREFIX=/home/cabbage/.PlayOnLinux/wineprefix/LeagueOfLegends/ wine '/home/cabbage/.PlayOnLinux/wineprefix/LeagueOfLegends/drive_c/Riot Games/League of Legends/lol.launcher.exe' 
-
-end
