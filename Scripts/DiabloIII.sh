5bd05f86779ad50723f963160a01f1137d5eb733
Lucas W. Bailey <lbaile200@gmail.com>
2014-11-14 11:21:03 -0500

    Delete DiabloIII.sh

--- Scripts/DiabloIII.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WINEPREFIX=/home/cabbage/Apps/Actual\ Files/battlenet/ setarch i386 -3 taskset -c 1 wine '/home/cabbage/Apps/Actual Files/battlenet/drive_c/Program Files (x86)/Diablo III/Diablo III.exe' -launch 
-
-end
