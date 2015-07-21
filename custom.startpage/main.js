11a269eab2be88984f25cf116ad218f50f2d4847
lbaile200 <lbaile200@gmail.com>
2015-07-21 01:33:22 -0400

    custom startpage

---
+++ custom.startpage/main.js
@@ -0,0 +1,40 @@
+
+function multi_open_work() {
+    window.open("https://ilsworld.zendesk.com");
+    window.open("https://crm.ilshome.com");
+    window.open("https://reg.marketplace6.com/home/admin/");
+    window.open("https://game.ilsworld.com");
+}
+
+function multi_open_news(){
+    window.open("https://reddit.com/r/news");
+    window.open("https://reddit.com/r/worldnews");
+    window.open("https://reddit.com/r/linux");
+    window.open("https://reddit.com/r/linux_gaming");
+    window.open("https://arstechnica.com");
+    window.open("https://linux.com");
+    window.open("https://phoronix.com");
+}
+
+function multi_open_self_improvement(){
+    window.open("https://artofmanliness.com");
+    window.open("https://lettersofnote.com");
+    window.open("https://eatthismuch.com");
+    window.open("http://fearlessmen.com");
+    window.open("https://goodmenproject.com");
+    window.open("http://insideofknoxville.com");
+}
+
+function multi_open_social(){
+    window.open("https://facebook.com");
+    window.open("https://instagram.com");
+    window.open("https://twitter.com");
+    window.open("https://plus.google.com");
+}
+
+function multi_open_web_comics(){
+  window.open("http://questionablecontent.net");
+  window.open("http://xkcd.com");
+  window.open("http://penny-arcade.com");
+  window.open("http://www.commitstrip.com/en/");
+}
